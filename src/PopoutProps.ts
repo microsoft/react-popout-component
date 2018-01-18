4181934cf83651d0eaa55c49ea436e393326bb74
Ken Chau <kchau@microsoft.com>
2018-01-18 14:56:35 -0800

    adding a onBlocked api

--- src/PopoutProps.ts
+++ src/PopoutProps.ts
@@ -1,10 +1,11 @@
-import { WindowFeaturesOptions } from "./WindwoFeaturesOptions";
+import { WindowFeaturesOptions } from './WindwoFeaturesOptions';
 
 export interface PopoutProps {
     hidden?: boolean;
     name?: string;
     onClose?: () => void;
     onBeforeUnload?: (evt: BeforeUnloadEvent) => string | null | undefined;
+    onBlocked?: () => void;
     children?: any;
     options?: Partial<WindowFeaturesOptions>;
     html?: string;
